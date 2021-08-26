3a9a2408f187535eff90f22c8cea54ccf52b0412
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-08-25 19:20:54 -0500

    Created first route, home, and set it to the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  # get 'home/index' => This was the original route at http://localhost:3000/home/index
+  root 'home#index' # Setting Root Route
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
