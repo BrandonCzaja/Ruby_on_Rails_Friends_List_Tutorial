1772366cadc33e1b883dcf74b50e8ec9dd343239
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-08-27 07:14:49 -0500

    Made the friends page the home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users # User authentication from Devise Gem
   resources :friends # Came from rails scaffold. Handles CRUD routes all from 'resources'
   # get 'home/index' => This was the original route at http://localhost:3000/home/index
-  root 'home#index' # Setting Root Route
+  root 'friends#index' # Setting Root Route. Inside the friends controller there is a file called index
   get 'home/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
